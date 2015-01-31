2f93ea7cfc43416d5a6cfb20fae5f9eb99b2e872
kcodes <kcodes14@gmail.com>
2015-01-31 04:45:20 -0500

    changes to CSS and JS

--- js/app.js
+++ js/app.js
@@ -1,17 +1,91 @@
-
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
 
-  	});
+  newGame();
+  /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+    });
 
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
+
+    /*Start new game*/
+    $(".new").click(function () {
+      newGame();
+      count = 0;
+      $("#count").text(count);
+      $("#guessList").empty();
+      $("input").prop('disabled', false);
+      $("#feedback").text("Make your Guess!");
+
+    });
+
+    /*Guess button*/
+    $("#guessButton").click(function(event) {
+      event.preventDefault();
+      userGuess = parseInt($("#userGuess").val());
+      if (userGuess >= 1 && userGuess <= 100)
+      {
+        getFeedback();
+        guessBox();
+        guessCounter();
+      }
+      else
+      {
+        alert("Please enter a number 1 - 100 ONLY!");
+      }
+      userGuess = $("#userGuess").val("");
+    });
 });
 
+var secretNumber;
+var userGuess;
+var feedback;
+var count = 0;
+var difference;
 
+var newGame = function () {
+    secretNumber = getSecretNumber();
+    console.log("Random Generated Secret Number: " + secretNumber); 
+};
+
+var getSecretNumber = function() {
+  
+  /*Create random number 1-100*/
+  return Math.floor(Math.random() * 100) + 1;
+};
+
+var getFeedback = function () {
+  difference = Math.abs(userGuess - secretNumber);
+  if (difference == 0) {
+    $("#feedback").text("You got it! Click new game to play again.");
+    $("input").prop('disabled', true);
+  }
+  else if (difference >= 50){
+    $("#feedback").text("ICE COLD, try again!");
+  }
+  else if (difference >= 30 && difference < 50){
+    $("#feedback").text("Cold, try again!");
+  }
+  else if (difference >= 20 && difference < 30){
+    $("#feedback").text("Warm, try again!");
+  }
+  else if (difference >= 10 && difference < 20){
+    $("#feedback").text("Hot, try again!");
+  }
+  else if (difference >= 1 && difference < 10){
+    $("#feedback").text("Burnin Hot, try again!");
+  }
+};
+
+var guessCounter = function() {
+  count++;
+  $("#count").text(count);
+};
+
+var guessBox = function() {
+  $("#guessList").append("<li>" + userGuess + "</li>");
+};
